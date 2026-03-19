bd9248d045eabd908dbbd2b8ab6a23dda255fa65
Zeyad Nafea <s-zeyad.nafea@zewailcity.edu.eg>
2026-03-19 15:32:48 +0200

    Add InternshipDetail route

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,42 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import ProtectedRoute from "./components/layout/ProtectedRoute";
 import AdminRoute from "./components/layout/AdminRoute";
-
 import Home from "./pages/Home/Home";
 import Courses from "./pages/Courses";
 import Dashboard from "./pages/Dashboard";
 import Internship from "./pages/Internship";
+import InternshipDetail from "./pages/InternshipDetail"; 
 import Notfound from "./pages/Notfound";
 import Profile from "./pages/Profile";
 import Workshop from "./pages/Workshop";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Siginup";
-import About from "./pages/About/About"
+import About from "./pages/About/About";
 import Contact from "./pages/Contact";
 import ScrollToTop from "./components/Scroll up/Scroll";
 
 function App() {
   return (
     <BrowserRouter>
-    <ScrollToTop/>
+      <ScrollToTop />
       <Routes>
         {/* With Header & Footer */}
         <Route element={<Layout />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
           {/* Public */}
           <Route path="/" element={<Home />} />
-           <Route path="/internship" element={<Internship />} />
-            <Route path="/workshop" element={<Workshop />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Contact" element={<Contact />} />
+          <Route path="/internship" element={<Internship />} />
+          <Route path="/internship/:id" element={<InternshipDetail />} /> 
+          <Route path="/workshop" element={<Workshop />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
 
           {/* Protected - logged in users */}
           <Route element={<ProtectedRoute />}>
-            
             <Route path="/profile" element={<Profile />} />
-           
           </Route>
 
           {/* Admin only */}
@@ -48,9 +47,7 @@ function App() {
 
           {/* 404 */}
           <Route path="*" element={<Notfound />} />
-
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
